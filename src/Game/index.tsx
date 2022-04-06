dd328d7805b59f6dbdc5770ca9816aff087a0a4c
inf0craw1 <inf0craw1@naver.com>
2022-04-06 23:31:23 +0900

    edit: get map data from mapData and location.map

--- src/Game/index.tsx
+++ src/Game/index.tsx
@@ -1,13 +1,23 @@
 import './game.scss';
 import RenderMap from "./Map";
+import User from "../components/user";
+import {mapData} from "../datas/map";
+import {usePlayerStore} from "../store/player";
+import {useEffect, useState} from "react";
 
 function Game() {
+  const { location } = usePlayerStore();
+  const [map, setMap] = useState<number[][]>(mapData[0]);
 
-  const map:number[][] = Array.from({length: 28}, () => (Array.from({length: 48}, () => 0)));
+  useEffect(() => {
+    setMap(mapData[location.map]);
+  }, [location.map]);
+
   return (
     <div className="window">
       <div className="display">
         <RenderMap mapData={map} />
+        <User/>
       </div>
     </div>
   )
