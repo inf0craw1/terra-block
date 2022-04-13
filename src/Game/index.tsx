17b7e9e2abbc1271eeb6df1d7d4b77c4306a0bbd
inf0craw1 <inf0craw1@naver.com>
2022-04-14 00:59:45 +0900

    remove:

--- src/Game/index.tsx
+++ src/Game/index.tsx
@@ -1,32 +1,28 @@
-import './game.scss';
+import "./game.scss";
 import RenderMap from "./Map";
 import Player from "../components/player";
-import {mapData} from "../datas/map";
-import {usePlayerStore} from "../store/player";
-import {useEffect, useState} from "react";
+import { mapData } from "../datas/map";
+import { usePlayerStore } from "../store/player";
+import { useEffect, useState } from "react";
 import Controller from "../components/controller";
 
 function Game() {
-  const { location, setLocationX } = usePlayerStore();
+  const { location } = usePlayerStore();
   const [map, setMap] = useState<number[][]>(mapData[0]);
 
   useEffect(() => {
     setMap(mapData[location.map]);
   }, [location.map]);
 
-  useEffect(() => {
-    console.log(`${location.x} / ${location.y}`);
-  }, [location]);
-
   return (
     <div className="window">
       <div className="display">
         <RenderMap mapData={map} />
-        <Player/>
-        <Controller/>
+        <Player />
+        <Controller />
       </div>
     </div>
-  )
+  );
 }
 
 export default Game;
