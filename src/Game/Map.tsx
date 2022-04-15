8608434a15709a6a573e19ea81c8292869643710
inf0craw1 <inf0craw1@naver.com>
2022-04-16 01:37:54 +0900

    edit: display targeted block(occurs rendering whole map issue)

--- src/Game/Map.tsx
+++ src/Game/Map.tsx
@@ -1,13 +1,35 @@
-import { RenderMapPropsInterface} from "./it";
+import { RenderMapPropsInterface } from "./it";
+import { usePlayerStore } from "../store/player";
 
-function RenderMap(props:RenderMapPropsInterface):JSX.Element {
+function RenderMap(props: RenderMapPropsInterface): JSX.Element {
   const { mapData } = props;
+  const { targetBlock } = usePlayerStore();
+
   return (
     <>
-      { mapData.map((row, rowIdx) => row.map((col, colIdx) => <img key={`map_${rowIdx}_${colIdx}`} src="/asset/img/map/grass.png" alt="" style={{position:'absolute', top: rowIdx*20, left:colIdx*20, width: 20, height:20  }}/>
-    ))}
+      {mapData.map((row, rowIdx) =>
+        row.map((col, colIdx) => (
+          <img
+            key={`map_${rowIdx}_${colIdx}`}
+            src="/asset/img/map/grass.png"
+            alt=""
+            className={`block ${
+              rowIdx === targetBlock.y &&
+              colIdx === targetBlock.x &&
+              "targeted-block"
+            }`}
+            style={{
+              position: "absolute",
+              top: rowIdx * 20,
+              left: colIdx * 20,
+              width: 20,
+              height: 20,
+            }}
+          />
+        ))
+      )}
     </>
-  )
+  );
 }
 
 export default RenderMap;
