85c1ecc65dc09fc469ee273926d64f3f7caabaf6
inf0craw1 <inf0craw1@naver.com>
2022-04-16 01:33:34 +0900

    add: useGameStore

---
+++ src/store/game/index.ts
@@ -0,0 +1,13 @@
+import create from "zustand";
+import { GameStoreInterface } from "./it";
+import { devtools } from "zustand/middleware";
+
+export const useGameStore = create<GameStoreInterface>(
+  devtools(() => ({
+    DISPLAY: {
+      width: 960,
+      height: 560,
+    },
+    BLOCK_SIZE: 20,
+  }))
+);
