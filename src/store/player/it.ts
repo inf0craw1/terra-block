331704f5db2fc873a429d9b09da0e0f7b084a5ed
inf0craw1 <inf0craw1@naver.com>
2022-04-16 01:35:43 +0900

    add: size, targetBlock state

--- src/store/player/it.ts
+++ src/store/player/it.ts
@@ -2,23 +2,33 @@ export interface PlayerStoreInterface {
   profile: {
     name: string;
     id: string;
-  },
+  };
   status: {
     HP: number;
     maxHP: number;
     speed: number;
-  },
+    direction: 1 | 2 | 3 | 4 | 6 | 7 | 8 | 9;
+  };
   location: {
     map: number;
     x: number;
     y: number;
-  },
+  };
+  size: {
+    width: number;
+    height: number;
+  };
+  targetBlock: {
+    x: number;
+    y: number;
+  };
   hand: number[];
   inventory: number[][];
-  setStatusHP: (HP:number) => void;
-  setStatusMaxHP: (maxHP:number) => void;
-  setStatusSpeed: (speed:number) => void;
-  setLocationMap: (map:number) => void;
-  setLocationX: (x:number) => void;
-  setLocationY: (y:number) => void;
+  setStatusHP: (HP: number) => void;
+  setStatusMaxHP: (maxHP: number) => void;
+  setStatusSpeed: (speed: number) => void;
+  setLocationMap: (map: number) => void;
+  setLocationX: (x: number) => void;
+  setLocationY: (y: number) => void;
+  setTargetBlock: (x: number, y: number) => void;
 }
