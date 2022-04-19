66c91407b16547716f08d551e7a175e497ba14b6
inf0craw1 <inf0craw1@naver.com>
2022-04-20 01:19:25 +0900

    add: processing time interfaces

--- src/store/game/it.ts
+++ src/store/game/it.ts
@@ -1,7 +1,14 @@
+export interface ProcessingTimeInterface {
+  item: number;
+  processingTime: number;
+}
 export interface GameStoreInterface {
   DISPLAY: {
     width: number;
     height: number;
   };
   BLOCK_SIZE: number;
+  PROCESSING_TIME: {
+    [item: number]: ProcessingTimeInterface;
+  };
 }
