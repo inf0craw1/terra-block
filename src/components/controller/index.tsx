73921b3ef3f84ee63a61850a3d6c0750f190eed9
inf0craw1 <inf0craw1@naver.com>
2022-04-20 01:17:47 +0900

    add: keyMap spacebar handleing edit: access object property change

--- src/components/controller/index.tsx
+++ src/components/controller/index.tsx
@@ -4,20 +4,26 @@ import { KeyMapInterface } from "../../store/contorller/it";
 import { useGameStore } from "../../store/game";
 
 const Controller = () => {
-  const { status, size, setLocationX, setLocationY, setStatusDirection } =
-    usePlayerStore();
+  const {
+    status,
+    size,
+    setLocationX,
+    setLocationY,
+    setStatusDirection,
+    targetBlock,
+  } = usePlayerStore();
   const keyMap: KeyMapInterface = {};
-  const { DISPLAY } = useGameStore();
+  const { DISPLAY, PROCESSING_TIME } = useGameStore();
 
   useEffect(() => {
     const interval = setInterval(() => {
       const loc = usePlayerStore.getState().location;
 
-      if (keyMap.ArrowLeft && !keyMap.ArrowRight) {
+      if (keyMap["ArrowLeft"] && !keyMap["ArrowRight"]) {
         setStatusDirection(4);
         setLocationX(loc.x - status.speed <= 0 ? 0 : loc.x - status.speed);
       }
-      if (keyMap.ArrowRight && !keyMap.ArrowLeft) {
+      if (keyMap["ArrowRight"] && !keyMap["ArrowLeft"]) {
         setStatusDirection(6);
         setLocationX(
           loc.x + status.speed >= DISPLAY.width - size.width
@@ -25,17 +31,21 @@ const Controller = () => {
             : loc.x + status.speed
         );
       }
-      if (keyMap.ArrowUp && !keyMap.ArrowDown) {
+      if (keyMap["ArrowUp"] && !keyMap["ArrowDown"]) {
         setStatusDirection(2);
         setLocationY(loc.y - status.speed <= 0 ? 0 : loc.y - status.speed);
       }
-      if (keyMap.ArrowDown && !keyMap.ArrowUp) {
+      if (keyMap["ArrowDown"] && !keyMap["ArrowUp"]) {
         setStatusDirection(8);
         setLocationY(
           loc.y + status.speed >= DISPLAY.height - size.height
             ? DISPLAY.height - size.height
             : loc.y + status.speed
         );
+      }
+      if (keyMap[" "]) {
+        if (targetBlock.item) {
+        }
       }
     }, 10);
 
