6ffd4e1f0027305e88921ee7746ac5a74d409f65
inf0craw1 <inf0craw1@naver.com>
2022-04-20 01:16:10 +0900

    add: processing time in game store

--- src/store/game/index.ts
+++ src/store/game/index.ts
@@ -2,12 +2,26 @@ import create from "zustand";
 import { GameStoreInterface } from "./it";
 import { devtools } from "zustand/middleware";
 
-export const useGameStore = create<GameStoreInterface>(
-  devtools(() => ({
+export const useGameStore = create(
+  devtools<GameStoreInterface>(() => ({
     DISPLAY: {
       width: 960,
       height: 540,
     },
     BLOCK_SIZE: 20,
+    PROCESSING_TIME: {
+      1: {
+        item: 1,
+        processingTime: 3,
+      },
+      2: {
+        item: 2,
+        processingTime: 2,
+      },
+      3: {
+        item: 3,
+        processingTime: 5,
+      },
+    },
   }))
 );
