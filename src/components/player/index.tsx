353d0a397153e3b7c0bf7673eea9196f96832be6
inf0craw1 <inf0craw1@naver.com>
2022-04-22 01:35:52 +0900

    add: player progressbar

--- src/components/player/index.tsx
+++ src/components/player/index.tsx
@@ -1,17 +1,35 @@
 import "./player.scss";
 import { usePlayerStore } from "../../store/player";
+import { useEffect } from "react";
 
 const Player = () => {
   const { profile, location } = usePlayerStore();
-
+  const targetBlock = usePlayerStore.getState().targetBlock;
+  useEffect(() => {
+    console.log(targetBlock.item);
+    console.log(targetBlock.process / targetBlock.processingTime);
+  }, [targetBlock.process]);
   return (
-    <div className="player" style={{
-      left: `${location.x}px`,
-      top: `${location.y}px`
-    }}>
-      <p className="name">{profile.name}</p>
+    <div
+      className="player"
+      style={{
+        left: `${location.x}px`,
+        top: `${location.y}px`,
+      }}
+    >
+      <div className="processBarWrapper">
+        <div
+          className="processBar"
+          style={{
+            width:
+              targetBlock.item && targetBlock.processingTime
+                ? (targetBlock.process / targetBlock.processingTime) * 40
+                : 0,
+          }}
+        ></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Player;
