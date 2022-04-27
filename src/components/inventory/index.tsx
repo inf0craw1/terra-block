a4476d9182f1773b45559d338e58cde3f1f751ac
inf0craw1 <inf0craw1@naver.com>
2022-04-28 00:39:06 +0900

    add: inventory component

---
+++ src/components/inventory/index.tsx
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+const Inventory = () => {
+  return (
+    <div className="inventory-wrapper">
+      <div className="inventory"></div>
+    </div>
+  );
+};
+
+export default Inventory;
