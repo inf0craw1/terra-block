116ece93d3595ecd682a6eaa848563086bd8ccf3
inf0craw1 <inf0craw1@naver.com>
2022-05-07 22:41:07 +0900

    edit: crafting-item

--- src/components/inventory/index.tsx
+++ src/components/inventory/index.tsx
@@ -3,7 +3,12 @@ import "./index.scss";
 const Inventory = () => {
   return (
     <div className="inventory-wrapper">
-      <div className="inventory"></div>
+      <div className="inventory">
+        <div className="craft-area">
+          div.crafting-item
+        </div>
+        <div className="items-area"></div>
+      </div>
     </div>
   );
 };
