57e14e27709e3058880117a9ad8d63c15fcc05b0
inf0craw1 <inf0craw1@naver.com>
2022-05-11 14:18:55 +0900

    add: crafting list area edit: inventory items renders with ItemBox component

--- src/components/inventory/index.tsx
+++ src/components/inventory/index.tsx
@@ -1,22 +1,37 @@
 import "./index.scss";
+import { usePlayerStore } from "../../store/player";
+import { craftList } from "../../datas/craftList";
+import ItemBox from "../itemBox";
 
 const Inventory = () => {
+  const inventory = usePlayerStore.getState().inventory;
   return (
     <div className="inventory-wrapper">
       <div className="inventory">
         <div className="craft-area">
-          <div className="crafting-item">
-            <div className="item"></div>
-            <div className="item"></div>
-            <div className="item"></div>
-            <div className="item"></div>
-          </div>
-          <div className="crafted-item">
-            <div className="item"></div>
+          <div className="crafting-list">
+            {craftList.map((l, lIdx) => {
+              <div
+                key={`craftingList${lIdx}`}
+                className={"crafting-list-itemBox"}
+              >
+                {l.using.map((using, uIdx) => {})}
+              </div>;
+            })}
           </div>
         </div>
         <div className="items-area">
-          <div className="items"></div>
+          <div className="items">
+            {inventory.items.map((row, rowIdx) =>
+              row.map((item, colIdx) => (
+                <ItemBox
+                  key={`inventory${rowIdx}${colIdx}`}
+                  item={item.code}
+                  quantity={item.quantity}
+                ></ItemBox>
+              ))
+            )}
+          </div>
         </div>
       </div>
     </div>
