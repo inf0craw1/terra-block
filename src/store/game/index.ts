57417b0aeb821cdb6776049f871d08b54a3b22e4
inf0craw1 <inf0craw1@naver.com>
2022-05-26 22:04:43 +0900

    add: objectMap, setMap func

--- src/store/game/index.ts
+++ src/store/game/index.ts
@@ -2,9 +2,42 @@ import create from "zustand";
 import { mapData } from "../../datas/map";
 import { devtools } from "zustand/middleware";
 import { GameStoreInterface } from "./it";
+import { objectMapData } from "../../datas/objectMap";
+import { usePlayerStore } from "../player";
+
+const location = usePlayerStore.getState().location;
 
 export const useGameStore = create<GameStoreInterface>(
   devtools((set) => ({
     map: mapData,
+    objectMap: objectMapData,
+    setMap: (rowIdx, colIdx, code) => {
+      set((state) => {
+        if (!code) {
+          if (!state.objectMap[location.map][rowIdx][colIdx]) {
+            const newMap = JSON.parse(JSON.stringify(state.objectMap));
+            newMap[location.map][rowIdx][colIdx] = 0;
+            return { ...state, objectMap: newMap };
+          }
+          if (!state.map[location.map][rowIdx][colIdx]) {
+            const newMap = JSON.parse(JSON.stringify(state.map));
+            newMap[location.map][rowIdx][colIdx] = 0;
+            return { ...state, map: newMap };
+          }
+        }
+        if (!state.map[location.map][rowIdx][colIdx]) {
+          const newMap = JSON.parse(JSON.stringify(state.map));
+          newMap[location.map][rowIdx][colIdx] = code;
+          return { ...state, map: newMap };
+        }
+        if (!state.objectMap[location.map][rowIdx][colIdx]) {
+          const newMap = JSON.parse(JSON.stringify(state.objectMap));
+          newMap[location.map][rowIdx][colIdx] = code;
+          return { ...state, objectMap: newMap };
+        }
+
+        return state;
+      });
+    },
   }))
 );
