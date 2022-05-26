4fbb521dd5eb99b676a8e62621083158310bb3ee
inf0craw1 <inf0craw1@naver.com>
2022-05-26 22:07:40 +0900

    add: objectMap, setMap func

--- src/store/game/it.ts
+++ src/store/game/it.ts
@@ -2,4 +2,6 @@ import { MapDataInterface } from "../../datas/map";
 
 export interface GameStoreInterface {
   map: MapDataInterface;
+  objectMap: MapDataInterface;
+  setMap: (rowIdx: number, colIdx: number, code: number) => void;
 }
