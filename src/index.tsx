0d8ddbe314e894b0e0550b98cc615b78243715fc
inf0craw1 <inf0craw1@naver.com>
2023-05-22 14:24:16 +0900

    refactor: indentation

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import './global.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import "./global.scss";
 
 const rootElement = document.getElementById("root");
 // @ts-ignore
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(<BrowserRouter><App /></BrowserRouter>);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
